9bd8098c62ef6888b85b04881e6d122cfbf84e86
Jose Alfonso <jose.alfonso@apple.com>
2023-01-07 14:29:29 -0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,56 @@
+import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        // 'vue-i18n',
+        // 'vue/macros',
+        // '@vueuse/head',
+        // '@vueuse/core',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        // 'src/composables/core/**',
+        // 'src/composables/collections',
+        // 'src/store',
+      ],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+      // resolvers for custom components
+      // resolvers: [
+      //   IconsResolver({
+      //     prefix: false,
+      //     alias: {
+      //       sy: 'symbol',
+      //       // icon: 'symbol',
+      //     },
+      //     customCollections: ['symbol', 'custom', 'icon'],
+      //   }),
+      // ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
